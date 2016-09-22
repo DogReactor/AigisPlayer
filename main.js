24aecfd91192ef80a3d6644ccd0475c7ea6ead9a
unknown <>
2016-09-22 18:37:39 +0800

    添加了一些奇怪的动画效果

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ function createWindow () {
   mainWindow.loadURL('file://' + __dirname + '/index.html');
   
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
   mainWindow.setMenu(null);
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
