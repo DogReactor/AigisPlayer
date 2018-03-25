ae7a5fa0c905af7988cd909e33e49e01270d2fdd
yukimir <luodadi.huazhu@gmail.com>
2018-03-25 16:42:07 +0800

    fix(fix hotkey bug and add a new hotkey service):

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import { ElectronService } from './electron.service'
 import { GameService } from './game.service'
 import { PluginService } from './plugin.service'
+import { HotkeyService } from './hotkey.service'
 
 
 @NgModule({
     imports: [CommonModule],
-    providers: [ElectronService, GameService, PluginService]
+    providers: [ElectronService, GameService, PluginService, HotkeyService]
 })
 export class CoreModule {
 
