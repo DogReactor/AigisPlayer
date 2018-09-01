39543a4d3f4048dc11a3f02b270079d28e6a25b1
DeltAlice <Etalice@outlook.com>
2018-09-01 18:52:55 +0800

    AL for ALTX&ALIG

--- src/app/decipher/AL.ts
+++ src/app/decipher/AL.ts
@@ -1,4 +1,5 @@
 import { BufferReader, Origin } from './BufferReader';
+import { bind1 } from '../../../node_modules/@angular/core/src/render3/instructions';
 
 interface AL {
     Buffer: Buffer;
@@ -357,6 +358,225 @@ namespace ALAR {
     }
 }
 
+class ALTX implements AL {
+    Buffer: Buffer;
+    Head: string;
+    Vers: number;
+    Form: number;
+    Count: number;
+    AligOffset: number;
+    Sprites = [];
+    RawImage={};
+    Width:number;
+    Height:number;
+
+    constructor(buffer: Buffer) {
+        this.Buffer = buffer;
+        const br = new BufferReader(buffer);
+        let startOffset = br.Position;
+        this.Head = br.ReadString(4);
+        this.Vers = br.ReadByte();
+        this.Form = br.ReadByte();
+        this.Count = br.ReadWord();
+        this.AligOffset = startOffset + br.ReadDword();
+        if (this.Form === 0) {
+            let blockStart = [];
+            for (let i=0;i<this.Count;++i){
+                blockStart.push(startOffset+br.ReadWord());
+            }
+            br.Align(4);
+            for(let i=0;i<this.Count;++i){
+                let frameName = '';
+                if(br.Position===blockStart[i] - 0x20){
+                    frameName=br.ReadString(0x20);
+                }
+                let index = br.ReadWord();
+                let temp = br.ReadWord();
+                let frames = br.ReadWord();
+                temp = br.ReadWord();
+                let frameTable = { name:frameName};
+                for (let j=0;j<frames;++j){
+                    let frame = {
+                        x:0,
+                        y:0,
+                        width:0,
+                        height:0,
+                        originX:0,
+                        originY:0
+                    };
+                    frame.x=br.ReadWord();
+                    frame.y=br.ReadWord();
+                    frame.width = br.ReadWord();
+                    frame.height = br.ReadWord();
+                    frameTable[j]=frame;
+                }
+                for (let j=0;j<frames;++j){
+                    frameTable[j].originX=br.ReadWord();
+                    frameTable[j].originY=br.ReadWord();
+                }
+                this.Sprites[index] = frameTable;
+            }
+        }
+        br.Position = this.AligOffset;
+
+        if (this.Form === 0) {
+            let rawimage = {};
+            const aligBuffer = br.ReadBytes(br.Length - br.Position);
+            this.RawImage = new ALIG(aligBuffer);
+        }
+        else if (this.Form === 0x0e){
+            this.Width = br.ReadWord();
+            this.Height = br.ReadWord();
+            this.RawImage = br.ReadString(0x100);
+        }
+
+    }
+    Package(path: string) {
+
+    }
+}
+
+class ALIG implements AL {
+    Buffer: Buffer;
+    Head: string;
+    Vers: number;
+    Form: string;
+    PaletteForm: string;
+    Count: number;
+    Width:number;
+    Height:number;
+    Size: number;
+    Palette = [];
+    PaletteSize = 0;
+    Image: string;
+
+    Unknown1:number;
+    Unknown2:number;
+    Unknown3:number;
+    Unknown5:number;
+    Unknown6:number;
+    Unknown7:number;
+
+    constructor(buffer: Buffer) {
+        this.Buffer = buffer;
+        const br = new BufferReader(buffer);
+        this.Head = br.ReadString(4);
+        this.Vers = br.ReadByte();
+        this.Unknown1 = br.ReadByte();
+        this.Unknown2 = br.ReadByte();
+        this.Unknown3 = br.ReadByte();
+
+        this.Form = br.ReadString(4);
+        this.PaletteForm = br.ReadString(4);
+        this.Width=br.ReadDword();
+        this.Height = br.ReadDword();
+        this.Size=this.Width*this.Height;
+        this.Unknown5=br.ReadWord();
+        this.Unknown6=br.ReadWord();
+        this.Unknown7=br.ReadDword();
+        
+        function extractChannel(pix:number, flag:number) {
+            let channel = pix%flag;
+            pix=(pix-channel)/flag;
+            return channel;
+        }
+        function convert(x) {
+            return Math.floor(x/8)*64+(x%8)*9;
+        }
+        let rawImage=[]
+        switch (this.Form) {
+            case 'PAL4':
+                this.PaletteSize = 16;
+                for (let i = 0; i < this.PaletteSize; ++i) {
+                    this.Palette[i] = br.ReadBytes(4);
+                    this.Palette[i].map(b => String.fromCharCode(b));
+                }
+                for (let i = 0; i < Math.floor(this.Size / 2); ++i) {
+                    let x = br.ReadByte();
+                    let low = x >> 4;
+                    let high = x & 0xf;
+                    rawImage.push(this.Palette[low] + this.Palette[high]);
+                }
+                break;
+            case 'ABG5':
+                for (let i = 0; i < this.Size; ++i) {
+                    let pix = br.ReadWord();
+                    let a = extractChannel(pix, 2);
+                    let b = extractChannel(pix, 32);
+                    let g = extractChannel(pix, 32);
+                    let r = extractChannel(pix, 32);
+                    r = convert(r);
+                    g = convert(g);
+                    b = convert(b);
+                    a = Math.floor(a * (255 / 1) + 0.5);
+                    rawImage.push(String.fromCharCode(r) + String.fromCharCode(g) + String.fromCharCode(b) + String.fromCharCode(a));
+                }
+                break;
+            case 'BGR5':
+                for (let i = 0; i < this.Size; ++i) {
+                    let pix = br.ReadWord();
+                    let b = extractChannel(pix, 32);
+                    let g = extractChannel(pix, 32);
+                    let r = extractChannel(pix, 32);
+                    let a = extractChannel(pix, 2);
+
+                    r = convert(r);
+                    g = convert(g);
+                    b = convert(b);
+                    a = Math.floor(a * (255 / 1) + 0.5);
+                    rawImage.push(String.fromCharCode(r) + String.fromCharCode(g) + String.fromCharCode(b) + String.fromCharCode(a));
+                }
+                break;
+            case 'ABG4':
+                for (let i = 0; i < this.Size; ++i) {
+                    let pix = br.ReadWord();
+                    let a = extractChannel(pix, 16);
+                    let b = extractChannel(pix, 16);
+                    let g = extractChannel(pix, 16);
+                    let r = extractChannel(pix, 16);
+
+                    r = Math.floor(r * (255 / 1) + 0.5);
+                    g = Math.floor(g * (255 / 1) + 0.5);
+                    b = Math.floor(b * (255 / 1) + 0.5);
+                    a = Math.floor(a * (255 / 1) + 0.5);
+                    rawImage.push(String.fromCharCode(r) + String.fromCharCode(g) + String.fromCharCode(b) + String.fromCharCode(a));
+                }
+                break;
+            case 'BGR4':
+                for (let i = 0; i < this.Size; ++i) {
+                    let pix = br.ReadWord();
+                    let b = extractChannel(pix, 16);
+                    let g = extractChannel(pix, 16);
+                    let r = extractChannel(pix, 16);
+                    let a = extractChannel(pix, 16);
+
+                    r = Math.floor(r * (255 / 1) + 0.5);
+                    g = Math.floor(g * (255 / 1) + 0.5);
+                    b = Math.floor(b * (255 / 1) + 0.5);
+                    a = Math.floor(a * (255 / 1) + 0.5);
+                    rawImage.push(String.fromCharCode(r) + String.fromCharCode(g) + String.fromCharCode(b) + String.fromCharCode(a));
+                }
+                break;
+            case 'RGBA':
+                br.ReadBytes(4 * this.Size).forEach(b => rawImage.push(String.fromCharCode(b)));
+                break;
+            case 'BGRA':
+                let p = br.ReadBytes(4 * this.Size);
+                for (let i = 0; i < p.length; i += 4) {
+                    let [b, g, r, a] = p.slice(i, 4).reverse();
+                    [r, g, b, a].forEach(b => rawImage.push(String.fromCharCode(b)));
+                }
+                break;
+                defaule: console.log("Unknwon image format: ", this.Form); break;
+        }
+        this.Image=rawImage.join('');
+
+    }
+    Package(path: string) {
+
+    }
+}
+
 function parseObject(buffer: Buffer) {
     const type = buffer.toString('utf-8', 0, 4);
     let r: AL;
@@ -374,6 +594,9 @@ function parseObject(buffer: Buffer) {
             break;
         case 'ALAR':
             r = new ALAR(buffer);
+            break;
+        case 'ALTX':
+            r = new ALTX(buffer);
             break;
         default:
             console.log('Not Support');
