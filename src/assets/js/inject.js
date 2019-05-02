fd91b65f7898ed12345757cc7daffe9e3a90ee1f
huazhu111 <luodadi.huazhu@gmail.com>
2019-05-02 17:57:50 +0800

    refactor(electron): update to 5.0.0

--- src/assets/js/inject.js
+++ src/assets/js/inject.js
@@ -1,3 +1,7 @@
+const remote = require("electron").remote;
+remote.app.commandLine.appendSwitch("js-flags", "--max-old-space-size=8192");
+ArrayBuffer = Buffer;
+
 global.ipcrender = require('electron').ipcRenderer;
 global.consoleLog = console.log;
 global.consoleError = console.error;
