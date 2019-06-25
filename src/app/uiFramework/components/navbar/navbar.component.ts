40d20b873ce08cce57feddc9a1256f2db0164627
orzyyyy <zy410419243@gmail.com>
2019-06-25 15:07:35 +0800

    feat: prompt mesasge to prevent misoperation before reload

--- src/app/uiFramework/components/navbar/navbar.component.ts
+++ src/app/uiFramework/components/navbar/navbar.component.ts
@@ -1,19 +1,23 @@
-import { Component } from '@angular/core';
-import { ElectronService } from '../../../core/electron.service'
+import { Component, Input } from '@angular/core';
+import { ElectronService } from '../../../core/electron.service';
 
 @Component({
-    selector: 'app-uiframe-navbar',
-    templateUrl: './navbar.component.html',
-    styleUrls: ['./navbar.component.scss']
+  selector: 'app-uiframe-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
 })
 export class UIFrameNavbarComponent {
-    toggle = false;
-    constructor(private electronService: ElectronService) {
+  @Input() dialogToggle: Function;
+
+  constructor(private electronService: ElectronService) {}
+
+  minimize() {
+    this.electronService.currentWindow.minimize();
+  }
+
+  toggle() {
+    if (this.dialogToggle) {
+      this.dialogToggle('navbar');
     }
-    minimize() {
-        this.electronService.currentWindow.minimize();
-    }
-    close() {
-        this.electronService.APP.exit(0);
-    }
+  }
 }
